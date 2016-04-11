1ed0354098d63d7e570bd5b5b531039f499ed77b
Michal <mstrasz@gmail.com>
2016-04-11 22:46:29 +0100

    indentation (spaces), Readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# slack_delete_files
-Batch delete files on Slack
+Batch delete files from Slack
+===================
+
+
+This node script allows you to batch delete files from the Slack archive
+
+----------
+
+
+Installation 
+-------------
+
+1. Download files / clone the repository
+1. Run `npm install`
+1. Edit the script and update `token` and `domain` variables
+1. Edit `dateLimit` variable (the default value is to delete all files that are older than 30 days)
+1. Run `node delete_old_files.js`
+1. Enjoy free space on Slack
+
+## Credits
+The code has been inspired by [Santiago L. Valdarrama's](https://www.shiftedup.com/2014/11/13/how-to-bulk-remove-files-from-slack) script in Python
--- delete_old_files.js
+++ delete_old_files.js
@@ -10,39 +10,39 @@ var apiURL = "https://" + domain + ".slack.com/api/";
 var dateLimit = new Date().getTime() - 30 * (1000 * 60 * 60 * 24); //30 days in milliseconds
 
 function getFiles(page, files) {
-	files = files || [];
-	page = page || 1;
-	console.log("Getting files. Page:", page);
-	request.post({url: apiURL + "files.list", form:{token: token, to_ts: dateLimit, page: page, count: 100}}, function(err, resp, body) {
-		var res = JSON.parse(resp.body);
-		var pages = res.paging.pages;
-		var currPage = res.paging.page;
-		var list = res.files;
-		if (list) {
-			files = files.concat(list);
-		}
+    files = files || [];
+    page = page || 1;
+    console.log("Getting files. Page:", page);
+    request.post({url: apiURL + "files.list", form: {token: token, to_ts: dateLimit, page: page, count: 100}}, function(err, resp, body) {
+        var res = JSON.parse(resp.body);
+        var pages = res.paging.pages;
+        var currPage = res.paging.page;
+        var list = res.files;
+        if (list) {
+            files = files.concat(list);
+        }
 
-		if (currPage < pages) {
-			getFiles(currPage + 1, files);
-		} else {
-			deleteFiles(files);
-		}
-	});
+        if (currPage < pages) {
+            getFiles(currPage + 1, files);
+        } else {
+            deleteFiles(files);
+        }
+    });
 }
 
 function deleteFiles(arr) {
-	console.log("Deleting %s files", arr.length);
-	arr = arr.map(function(item) {
-		return function(callback) {
-			console.log("Deleting a file:", item.name);
-			request.post({url: apiURL + "files.delete", form:{token: token, file: item.id}}, function(err, resp, body) {
-				callback();
-			});
-		}
-	});
-	async.parallel(arr, function(err) {
-		console.log("Done");
-	})
+    console.log("Deleting %s files", arr.length);
+    arr = arr.map(function(item) {
+        return function(callback) {
+            console.log("Deleting a file:", item.name);
+            request.post({url: apiURL + "files.delete", form:{token: token, file: item.id}}, function(err, resp, body) {
+                callback();
+            });
+        }
+    });
+    async.parallel(arr, function(err) {
+        console.log("Done");
+    })
 }
 
 getFiles();
